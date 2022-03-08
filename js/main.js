581cd9da1f7bb65df8cce32b318abab3501d0521
kowalst <kowalst@wwu.edu>
2022-03-08 15:57:46 -0800

    modal js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    /* lightbox */
-    baguetteBox.run('.gallery', {
-        animation: 'fadeIn'
-    });
-});
+//$(document).ready(function(){
+//    /* lightbox */
+//    baguetteBox.run('.gallery', {
+//        animation: 'fadeIn'
+//    });
+//});
 
 function formValidator(){
     var name = document.getElementById('name').value;
@@ -53,7 +53,50 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+//  slides[slideIndex-1].style.display = "block";
+//  dots[slideIndex-1].className += " active";
+//  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+// Get the modal
+var modal0 = document.getElementById("myModal0");
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var smimg0 = document.getElementById("smimg0");
+var smimg1 = document.getElementById("smimg1");
+var smimg2 = document.getElementById("smimg2");
+
+var modalImg0 = document.getElementById("img0");
+var modalImg1 = document.getElementById("img1");
+var modalImg2 = document.getElementById("img2");
+
+smimg0.onclick = function(){
+  modal0.style.display = "block";
+  modalImg0.src = "images/datav0.png";
+}
+smimg1.onclick = function(){
+    modal1.style.display = "block";
+    modalImg1.src = "images/datav1.png";
+}
+smimg2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = "images/datav2.png";
+}
+// Get the <span> element that closes the modal
+var span0 = document.getElementsByClassName("close0")[0];
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span0.onclick = function() {
+    modal0.style.display = "none";
+} 
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
 }
